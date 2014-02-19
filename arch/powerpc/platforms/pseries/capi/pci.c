05091d5c5b9719d200f97b1f585d2a1599e5c5bb
Ian Munsie <imunsie@au1.ibm.com>
2014-02-19 18:16:50 +1100

    capi pci wip

--- arch/powerpc/platforms/pseries/capi/pci.c
+++ arch/powerpc/platforms/pseries/capi/pci.c
@@ -6,15 +6,20 @@
 #include <linux/of.h>
 #include <asm/opal.h>
 
+#define CAPI_PCI_VSEC_ID	0xb
+
+#define CAPI_PROTOCOL_256TB	(1ull << (23-7))
+#define CAPI_PROTOCOL_ENABLE	(1ull << (23-16))
+
 DEFINE_PCI_DEVICE_TABLE(capi_pci_tbl) = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_IBM, 0x0477), },
-	{ PCI_DEVICE_CLASS(0x120000, ~0), },
+	{ PCI_DEVICE_CLASS(0x1200FF, ~0), },
 
 	{ }
 };
 MODULE_DEVICE_TABLE(pci, capi_pci_tbl);
 
-int switch_to_capi(struct pci_dev *dev)
+static int switch_phb_to_capi(struct pci_dev *dev)
 {
 	struct device_node *np;
 	struct property *prop = NULL;
@@ -47,15 +52,61 @@ out:
 	return rc;
 }
 
+static int switch_card_to_capi(struct pci_dev *dev)
+{
+	int vsec;
+	u32 val;
+	int rc;
+
+	pr_info("capi: switch card to capi\n");
+
+	if ((vsec = pci_find_ext_capability(dev, CAPI_PCI_VSEC_ID)) == 0) {
+		pr_err("capi: No CAPI VSEC found!\n");
+		return -ENODEV;
+	}
+
+	pr_info("capi vsec found at offset %#x\n", vsec);
+
+	/* FIXME: Can probably just read/write one byte and not worry about the
+	 * number of AFUs and status fields */
+	if ((rc = pci_read_config_dword(dev, vsec + 0x8, &val))) {
+		pr_err("capi: failed to read current mode control: %i", rc);
+		return rc;
+	}
+	/* FIXME: Clear other protocol size bits */
+	val |= CAPI_PROTOCOL_256TB | CAPI_PROTOCOL_ENABLE;
+	if ((rc = pci_write_config_dword(dev, vsec + 0x8, val))) {
+		pr_err("capi: failed to enable capi protocol: %i", rc);
+		return rc;
+	}
+
+	return 0;
+}
+
+int enable_capi_protocol(struct pci_dev *dev)
+{
+	int rc;
+
+	/* XXX: Which needs to happen first? */
+
+	if ((rc = switch_phb_to_capi(dev)))
+		return rc;
+
+	return switch_card_to_capi(dev);
+}
+
 int capi_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
 	int rc;
 
 	pr_info("capi pci probe\n");
 
-	switch_to_capi(dev);
+	if ((rc = enable_capi_protocol(dev))) {
+		pr_err("capi-pci: enable_capi_protocol failed: %i\n", rc);
+		return rc;
+	}
 
-	/* FIXME: Should wait for PHB to come back in CAPI mode and re-probe */
+	/* FIXME: Should I wait for PHB to come back in CAPI mode and re-probe? */
 	if ((rc = pci_enable_device(dev))) {
 		pr_err("capi-pci: pci_enable_device failed: %i\n", rc);
 		return rc;
