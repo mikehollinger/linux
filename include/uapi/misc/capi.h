f16606ae34f4c1f087a8382f843e77e65db678a9
Ian Munsie <imunsie@au1.ibm.com>
2014-06-18 15:24:51 +1000

    Rework userspace API - header changes only (won't compile)

--- include/uapi/misc/capi.h
+++ include/uapi/misc/capi.h
@@ -3,11 +3,8 @@
 
 #include <linux/types.h>
 
-#define AFU_PS_REGS_SIZE 0x2000000
-
 /* ioctls */
 
-/* Argument *is* WED (DEPRECATED: 32bit applications cannot set full 64bit WED) */
 /* Argument is a pointer to a struct capi_ioctl_start_work */
 #define CAPI_IOCTL_START_WORK      0
 #define CAPI_IOCTL_LOAD_AFU_IMAGE  1
@@ -17,8 +14,12 @@ struct capi_ioctl_start_work {
 	__u64 amr;
 	__u64 ctx_save_ptr; /* Ignored in dedicated process model */
 	__u32 ctx_save_size; /* In bytes */
-	__u32 reserved1;
+	__s16 num_interrupts; /* -1 = use value from afu descriptor */
+	__u16 process_element; /* returned from kernel */
+	__u64 reserved1;
 	__u64 reserved2;
+	__u64 reserved3;
+	__u64 reserved4;
 };
 
 struct capi_ioctl_load_afu_image {
@@ -29,41 +30,55 @@ struct capi_ioctl_load_afu_image {
 /* events from read() */
 
 enum capi_event_type {
-	CAPI_EVENT_READ_FAIL = -1,
-	CAPI_EVENT_RESERVED = 0,
+	CAPI_EVENT_READ_FAIL     = -1,
+	CAPI_EVENT_RESERVED      = 0,
 	CAPI_EVENT_AFU_INTERRUPT = 1,
-	CAPI_EVENT_DATA_STORAGE = 2,
-	CAPI_EVENT_AFU_ERROR = 3,
+	CAPI_EVENT_DATA_STORAGE  = 2,
+	CAPI_EVENT_AFU_ERROR     = 3,
 };
 
 struct capi_event_header {
 	__u32 type;
 	__u16 size;
-	__u16 reserved;
+	__u16 process_element;
+	__u64 reserved1;
+	__u64 reserved2;
+	__u64 reserved3;
 };
 
 /* Keep this large enough to fit any event for convenience */
 struct capi_event_uncast {
 	struct capi_event_header header;
-	__u8 data[8];
+	__u64 reserved1;
+	__u64 reserved2;
+	__u64 reserved3;
+	__u64 reserved4;
 };
 
-#define AFU_IRQ_LEVEL_1 0x01
-#define AFU_IRQ_LEVEL_2 0x02
-#define AFU_IRQ_LEVEL_3 0x04
 struct capi_event_afu_interrupt {
 	struct capi_event_header header;
-	__u8 level; /* Raised AFU interrupt level(s) */
+	__u16 irq; /* Raised AFU interrupt number */
+	__u16 reserved1;
+	__u32 reserved2;
+	__u64 reserved3;
+	__u64 reserved4;
+	__u64 reserved5;
 };
 
 struct capi_event_data_storage {
 	struct capi_event_header header;
 	__u64 address;
+	__u64 reserved1;
+	__u64 reserved2;
+	__u64 reserved3;
 };
 
 struct capi_event_afu_error {
 	struct capi_event_header header;
 	__u64 afu_err;
+	__u64 reserved1;
+	__u64 reserved2;
+	__u64 reserved3;
 };
 
 #endif
