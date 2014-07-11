27a4e5f5255729f8d6e5411f178dce7ce7c5ce17
Ian Munsie <imunsie@au1.ibm.com>
2014-07-11 13:36:36 +1000

    Tweak capi & afu naming yet again
    - Rename /sys/class/caccel to /sys/class/cxl (as in Coherent acceleator)
    - Move device nodes under a subdirectory /dev/cxl/
    - Rename card device nodes to card%i
    - Rename afu device nodes to afu%i.%i (card.afu)
    - Rename afu master device nodes to afu%i.%im

--- drivers/misc/capi/capi.h
+++ drivers/misc/capi/capi.h
@@ -410,6 +410,7 @@ struct capi_t {
 	struct cdev afu_cdev;
 	struct cdev afu_master_cdev;
 	struct device device;
+	int adapter_num;
 	int slices;
 	struct dentry *trace;
 	struct dentry *psl_err_chk;
--- drivers/misc/capi/file.c
+++ drivers/misc/capi/file.c
@@ -724,7 +724,7 @@ int add_capi_afu_dev(struct capi_afu_t *afu, int slice)
 	/* FIXME check afu->pp_mmio to see if we need this file */
 	afu->device.parent = &afu->adapter->device;
 	afu->device.class = capi_class;
-	dev_set_name(&afu->device, "%s%i", dev_name(&afu->adapter->device), slice + 1);
+	dev_set_name(&afu->device, "afu%i.%i", afu->adapter->adapter_num, slice);
 	afu->device.devt = MKDEV(capi_major, capi_minor + CAPI_MAX_SLICES + 1 + slice);
 	afu->device.class = capi_class;
 	afu->device.release = capi_release;
@@ -743,7 +743,7 @@ int add_capi_afu_dev(struct capi_afu_t *afu, int slice)
 	/* Add the AFU master device */
 	afu->device_master.parent = &afu->device;
 	afu->device_master.class = capi_class;
-	dev_set_name(&afu->device_master, "%s%im", dev_name(&afu->adapter->device), slice + 1);
+	dev_set_name(&afu->device_master, "afu%i.%im", afu->adapter->adapter_num, slice);
 	afu->device_master.class = capi_class;
 	afu->device_master.devt = MKDEV(capi_major, capi_minor + 1 + slice);
 	afu->device_master.release = capi_release;
