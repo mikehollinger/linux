7f8717d3508a098510e68882b8c7dec9e927e1b7
Ian Munsie <imunsie@au1.ibm.com>
2014-08-27 13:32:23 +1000

    Change the big scary m64 warning to a little less scary info ;)

--- drivers/misc/cxl/pci.c
+++ drivers/misc/cxl/pci.c
@@ -453,10 +453,12 @@ static void reassign_cxl_bars(struct pci_dev *dev)
 	}
 
 	/*
-	 * MASSIVE HACK: CXL requires the m64 address space for BAR
-	 * assignment. Our PHB code in Linux doesn't use it yet, and Linux will
-	 * have assigned BARs from the m32 space. For now just reassign the
-	 * BARs from the m64 space.
+	 * CXL requires the m64 address space for BAR assignment. Our PHB code
+	 * in Linux doesn't use it yet, and Linux will have assigned BARs from
+	 * the m32 space. This code reassigns the BARs from the m64 space,
+	 * which is OK since the CAPI card can be the only thing behind the
+	 * PHB so it won't ever be assigned to anything else. Later when Linux
+	 * can assign BARs from the m64 space we can use that instead.
 	 */
 	window_prop = of_get_property(np, "ibm,opal-m64-window", NULL);
 	if (!window_prop) {
