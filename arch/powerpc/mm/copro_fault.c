eb3798145ad5711b63fcaba04946492b578b7d3e
Michael Neuling <mikey@neuling.org>
2014-10-07 17:00:40 +1100

    move calculate_vsid back into copro_calc_slb

--- arch/powerpc/mm/copro_fault.c
+++ arch/powerpc/mm/copro_fault.c
@@ -96,13 +96,37 @@ EXPORT_SYMBOL_GPL(copro_handle_mm_fault);
 int copro_calc_slb(struct mm_struct *mm, u64 ea, struct copro_slb *slb)
 {
 	u64 vsid;
-	int psize, ssize, rc;
+	int psize, ssize;
 
 	slb->esid = (ea & ESID_MASK) | SLB_ESID_V;
 
-	rc = calculate_vsid(mm, ea, &vsid, &psize, &ssize);
-	if (rc)
-		return rc;
+	switch (REGION_ID(ea)) {
+	case USER_REGION_ID:
+		pr_devel("%s: 0x%llx -- USER_REGION_ID\n", __func__, ea);
+		psize = get_slice_psize(mm, ea);
+		ssize = user_segment_size(ea);
+		vsid = get_vsid(mm->context.id, ea, ssize);
+		break;
+	case VMALLOC_REGION_ID:
+		pr_devel("%s: 0x%llx -- VMALLOC_REGION_ID\n", __func__, ea);
+		if (ea < VMALLOC_END)
+			psize = mmu_vmalloc_psize;
+		else
+			psize = mmu_io_psize;
+		ssize = mmu_kernel_ssize;
+		vsid = get_kernel_vsid(ea, mmu_kernel_ssize);
+		break;
+	case KERNEL_REGION_ID:
+		pr_devel("%s: 0x%llx -- KERNEL_REGION_ID\n", __func__, ea);
+		psize = mmu_linear_psize;
+		ssize = mmu_kernel_ssize;
+		vsid = get_kernel_vsid(ea, mmu_kernel_ssize);
+		break;
+	default:
+		pr_debug("%s: invalid region access at %016llx\n", __func__, ea);
+		return 1;
+	}
+
 	vsid = (vsid << slb_vsid_shift(ssize)) | SLB_VSID_USER;
 
 	vsid |= mmu_psize_defs[psize].sllp |
