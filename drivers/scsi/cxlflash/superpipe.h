34e81134415c1af4b47992ced2147dc06bc9553f
Uma Krishnan <ukrishn@linux.vnet.ibm.com>
2017-01-03 14:20:47 -0600

    Revert "SAUCE: cxlflash: Add kref to context"
    This reverts commit 5081937a165fd7ad0923856375732b4daf7cfa12.

--- drivers/scsi/cxlflash/superpipe.h
+++ drivers/scsi/cxlflash/superpipe.h
@@ -106,7 +106,6 @@ struct ctx_info {
 	bool unavail;
 	bool err_recovery_active;
 	struct mutex mutex; /* Context protection */
-	struct kref kref;
 	struct cxl_context *ctx;
 	struct cxlflash_cfg *cfg;
 	struct list_head luns;	/* LUNs attached to this context */
