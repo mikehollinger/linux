b1159b445bd65e4ac8d9e4543d361d0b3e741dbd
Uma Krishnan <ukrishn@linux.vnet.ibm.com>
2017-08-08 16:45:46 -0500

    Revert "SAUCE: scsi: cxlflash: Hide FC internals behind common access routine"
    This reverts commit 194a88028c1493394da56a28982debfdfb21b2b5.

--- drivers/scsi/cxlflash/vlun.c
+++ drivers/scsi/cxlflash/vlun.c
@@ -821,8 +821,9 @@ void cxlflash_restore_luntable(struct cxlflash_cfg *cfg)
 	struct llun_info *lli, *temp;
 	u32 lind;
 	int k;
+	struct afu *afu = cfg->afu;
 	struct device *dev = &cfg->dev->dev;
-	__be64 __iomem *fc_port_luns;
+	struct sisl_global_map __iomem *agm = &afu->afu_map->global;
 
 	mutex_lock(&global.mutex);
 
@@ -835,8 +836,8 @@ void cxlflash_restore_luntable(struct cxlflash_cfg *cfg)
 
 		for (k = 0; k < cfg->num_fc_ports; k++)
 			if (lli->port_sel & (1 << k)) {
-				fc_port_luns = get_fc_port_luns(cfg, k);
-				writeq_be(lli->lun_id[k], &fc_port_luns[lind]);
+				writeq_be(lli->lun_id[k],
+					  &agm->fc_port[k][lind]);
 				dev_dbg(dev, "\t%d=%llx\n", k, lli->lun_id[k]);
 			}
 	}
@@ -876,8 +877,9 @@ static int init_luntable(struct cxlflash_cfg *cfg, struct llun_info *lli)
 	u32 nports;
 	int rc = 0;
 	int k;
+	struct afu *afu = cfg->afu;
 	struct device *dev = &cfg->dev->dev;
-	__be64 __iomem *fc_port_luns;
+	struct sisl_global_map __iomem *agm = &afu->afu_map->global;
 
 	mutex_lock(&global.mutex);
 
@@ -913,8 +915,7 @@ static int init_luntable(struct cxlflash_cfg *cfg, struct llun_info *lli)
 			if (!(lli->port_sel & (1 << k)))
 				continue;
 
-			fc_port_luns = get_fc_port_luns(cfg, k);
-			writeq_be(lli->lun_id[k], &fc_port_luns[lind]);
+			writeq_be(lli->lun_id[k], &agm->fc_port[k][lind]);
 			dev_dbg(dev, "\t%d=%llx\n", k, lli->lun_id[k]);
 		}
 
@@ -931,8 +932,7 @@ static int init_luntable(struct cxlflash_cfg *cfg, struct llun_info *lli)
 		}
 
 		lind = lli->lun_index = cfg->last_lun_index[chan];
-		fc_port_luns = get_fc_port_luns(cfg, chan);
-		writeq_be(lli->lun_id[chan], &fc_port_luns[lind]);
+		writeq_be(lli->lun_id[chan], &agm->fc_port[chan][lind]);
 		cfg->last_lun_index[chan]--;
 		dev_dbg(dev, "%s: Virtual LUNs on slot %d:\n\t%d=%llx\n",
 			__func__, lind, chan, lli->lun_id[chan]);
